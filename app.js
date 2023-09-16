be52047faa890046a47a11acfb7d29f27a530d65
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-09-16 18:49:30 +0200

    add public goal images

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const routes = require("./routes");
 const cors = require("cors");
-
+const path = require("node:path");
 const app = express();
 
 app.use(cors());
+
+app.use("/goal", express.static(path.join(__dirname, "public")));
 
 app.use("/api", routes);
 
 app.use((req, res) => res.status(404).send({ message: "Not found" }));
 
 app.use((err, req, res, next) => {
-	const { status = 500, message = "Internal server error" } = err;
+  const { status = 500, message = "Internal server error" } = err;
 
-	return res.status(status).send({ message });
+  return res.status(status).send({ message });
 });
 
 module.exports = app;
