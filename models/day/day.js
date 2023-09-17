2d07ed8f89315c653111d17b81f4d8beb54fbd5e
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-09-17 21:16:01 +0300

    day

--- models/day/day.js
+++ models/day/day.js
@@ -1,11 +1,17 @@
 const current = require('../../controllers/user/current');
 const Day = require('../../schemas/day');
-const formatDate = require('../../utils/dateutils');
+// const formatDate = require('../../utils/dateutils');
 
 const currentDate = new Date();
-console.log(formatDate(currentDate));
+// console.log(formatDate(currentDate));
+const startOfDay = new Date(currentDate.toISOString().split('T')[0]);
+const endOfDay = new Date(startOfDay);
+endOfDay.setHours(23, 59, 59, 999);
 
-const daysInfo = async(req) => {
+const daysInfo = async () => {
+    const dayInfo = await Day.find({ date: { $gte: startOfDay, $lte: endOfDay } });
+
+    return dayInfo;
     // const userId = req.user.id;
     // return await Day.find({ ownerId: userId, date: currentDate });
 };
@@ -20,7 +26,7 @@ const createDays = async (req, body) => {
 };
 
 const updateDays = async (dateToUpdate, updatedData) => {
-    const updatedDay = await Day.findOneAndUpdate({ date: dateToUpdate }, { $set: updatedData }, { new: true });
+    const updatedDay = await Day.findOneAndUpdate({ date: { $gte: startOfDay, $lte: endOfDay } });
 
     if (!updatedDay) {
             return null;
