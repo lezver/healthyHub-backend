752667a38f96bd412384752e8c0e658f780906f0
Vladislav <lezver1994@gmail.com>
2023-09-21 20:40:29 +0300

    update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = process.env;
+const { DB_HOST, HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(80);
-    console.log("Server  running. Use our API on port: 80");
-  })
-  .catch((err) => {
-    console.error(err.message);
+	.connect(DB_HOST)
+	.then(() => {
+		app.listen(HOST);
+		console.log(`Server  running. Use our API on port: ${HOST}`);
+	})
+	.catch((err) => {
+		console.error(err.message);
 
-    process.exit(1);
-  });
+		process.exit(1);
+	});
