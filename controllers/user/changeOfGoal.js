664d092e942ee8a9dafc27fb0cd996e5baebc352
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-09-21 17:10:33 +0200

    update swagger

--- controllers/user/changeOfGoal.js
+++ controllers/user/changeOfGoal.js
@@ -2,16 +2,16 @@ const { wrapperError, httpError } = require("../../helpers");
 const { User } = require("../../models/user");
 
 const changeOfGoal = async (req, res) => {
-	const { _id } = req.user;
-	const { goal } = req.body;
+  const { _id } = req.user;
+  const { goal } = req.body;
 
-	const changeUserGoal = await User.findByIdAndUpdate(
-		_id,
-		{ goal },
-		{ new: true }
-	).exec();
+  const changeUserGoal = await User.findByIdAndUpdate(
+    _id,
+    { goal },
+    { new: true }
+  ).exec();
 
-	return res.status(200).send({ goal: changeUserGoal.goal });
+  return res.status(200).send({ goal: changeUserGoal.goal });
 };
 
 module.exports = { changeOfGoal: wrapperError(changeOfGoal) };
