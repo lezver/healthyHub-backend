2e5bee59015627435173501def139b58aa8f9315
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-09-26 19:03:14 +0300

    fix

--- schemas/day.js
+++ schemas/day.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
 const daySchema = new mongoose.Schema({
-    date: Date,
-    calories: Number,    
+    date: Date,  
     water: Number,
     weight: Number,
-    carbonohidrates: Number,
-    protein: Number,
-    fat: Number,
-    calories: Number,
     isChanged: {
         type: Boolean,
         default: false,
